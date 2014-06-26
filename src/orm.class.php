5390725bade6470dbce55824c194deb2ece9e58f
Sercan Dağlı <sdagli7@gmail.com>
2014-06-26 22:55:15 +0300

    create constructor

--- src/orm.class.php
+++ src/orm.class.php
@@ -1,1 +1,23 @@
 <?php
+require_once 'config.php';
+/*
+ * Simple Php Orm For Mysql
+ * 
+ * @author Sercan Dağlı	<sdagli7@gmail.com>
+ */
+
+/*
+ * ORM class for all actions 
+ */
+class ORM{
+	private $connection;
+	
+	public function __construct(){
+		$connection_string = "mysql:host=" . HOST . ";dbname=" . DBNAME . ";";
+		try{
+			$this->connection = new PDO($connection_string, USERNAME, PASSWORD);
+		}catch (PDOException $e){
+			echo $e;
+		}	
+	}
+} 
